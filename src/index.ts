709b1d72dbe7807d1f0bb0369dc76c65743e4680
tvaulin <tvaulin@ozon.ru>
2023-10-13 00:33:16 +0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 
 import express from 'express'
+import cors from 'cors';
 import {InversifyExpressServer} from "inversify-express-utils";
 import {container} from "./di/inversify.config";
 import errorHandlerMiddleware from "./middlewares/ErrorHandlerMiddleware";
@@ -9,6 +10,7 @@ let server = new InversifyExpressServer(container);
 server.setConfig((app) => {
     app.use(express.json())
     app.use(express.urlencoded())
+    app.use(cors())
 })
 
 server.setErrorConfig((app) => {
